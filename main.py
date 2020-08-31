aa7bfdf0ff6d4e2c553584fdb5ce242078c340a4
artem <ivashenco45@gmail.com>
2020-08-31 14:19:34 +0300

    [init] create project and add 1 command handler [voice_to_str]

---
+++ main.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+import os
+import tools
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+app = Client(session_name='session', api_hash=api_hash, api_id=api_id)
+
+
+@app.on_message(filters.me & filters.reply & filters.command('voice_to_str'))
+def voice_to_str_handle(client, message):
+    print(message)
+    print(message.reply_to_message.voice)
+    if message.reply_to_message.voice:
+        client.download_media(message.reply_to_message, file_name='voice.ogg')
+        voice_text = tools.voice_to_str()
+        client.send_message(message.chat.id, 'Текст: ' + voice_text)
+    else:
+        client.send_message(message.chat.id, 'Голосовое сообщение не найдено.')
+
+
+app.run()
---
+++ tools.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import subprocess
+import os
+
+
+def ogg_to_wav(voice_path):
+    try:
+        os.remove('new_voice.wav')
+    except FileNotFoundError:
+        pass
+
+    subprocess.run(['ffmpeg', '-i', voice_path, 'new_voice.wav'])
+
+
+def voice_to_str(voice_path='downloads/voice.ogg'):
+    ogg_to_wav(voice_path)
+    r = sr.Recognizer()
+    with sr.WavFile("new_voice.wav") as source:  # use "test.wav" as the audio source
+        audio = r.record(source)
+        return r.recognize_google(audio, language='ru')
+
+
